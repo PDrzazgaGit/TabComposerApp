107ba10ffa46ee40d9cdb08ce44468a7550ddcb5
PDrzazgaGit <151642787+PDrzazgaGit@users.noreply.github.com>
2024-10-10 12:13:10 +0200

    Navbar added

---
+++ tabcomposerapp.client/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                <h1>
+                    All rights reserved
+                </h1>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
+
---
+++ tabcomposerapp.client/src/components/MyNavbar.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { Navbar, Nav , Container, Form, InputGroup } from "react-bootstrap";
+
+export const MyNavbar = () => {
+    
+    return (
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand>
+                    <Nav.Link as={Link} to="/">TabComposer</Nav.Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                <Navbar.Collapse className="mt-2" id="responsive-navbar-nav">
+
+                    <Form className="ms-auto d-flex">
+                        <InputGroup>
+                            <Form.Control
+                                type="search"
+                                placeholder="Search for Tabs"
+                                className="me-2"
+                                aria-label="Search for Tabs"
+                            />
+                            <InputGroup.Text>
+                                <i className="bi bi-search"></i>
+                            </InputGroup.Text>
+                        </InputGroup>
+                    </Form>
+
+                    <Nav className="ms-auto d-flex align-items-center">
+                        <Nav className="me-auto ">
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/login">Sign in</Nav.Link>
+                        </Nav>
+                    </Nav>
+                    
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
